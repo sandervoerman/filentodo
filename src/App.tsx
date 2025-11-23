c9becfb5479c92d43e41c6dd7b435bc920845449
Sander Voerman <sander@savoerman.nl>
2025-11-23 17:06:45 +0100

    Global layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import type { Task } from './model/tasks';
 import { useReducer } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { Container } from '@mui/material';
+import { Container, CssBaseline, Box } from '@mui/material';
+
+import type { Task } from './model/tasks';
 import { taskReducer } from './model/tasks';
-import { Bar } from './Bar';
+import { Header } from './Header';
 import { TaskListPanel } from './workspace/TaskListPanel';
+import { Footer } from './Footer';
 
 
 const darkTheme = createTheme({
@@ -20,6 +21,10 @@ const initialTasks: Task[] = [
   { id: '2', title: 'Sample Task', completed: false, contexts: ['work', 'home'], projects: [] },
   { id: '3', title: 'Sample Task', completed: false, contexts: [], projects: [] },
   { id: '4', title: 'Sample Task', completed: false, contexts: ['home'], projects: [] },
+  { id: '5', title: 'Sample Task', completed: false, contexts: [], projects: [] },
+  { id: '6', title: 'Sample Task', completed: false, contexts: ['home'], projects: [] },
+  { id: '7', title: 'Sample Task', completed: false, contexts: [], projects: [] },
+  { id: '8', title: 'Sample Task', completed: false, contexts: ['home'], projects: [] },
 ];
 
 
@@ -29,14 +34,25 @@ function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <Container disableGutters>
-        <Bar />
-        <TaskListPanel
-          tasks={tasks}
-          onCreateTask={(details) => dispatch({ type: 'create', details })}
-          onUpdateTask={(task) => dispatch({ type: 'update', task })}
-          onDeleteTask={(id) => dispatch({ type: 'delete', id })}
-        />
+      <Container disableGutters sx={{
+        height: '100vh',
+        border: '1px solid red',
+        display: 'flex',
+        flexDirection: 'column',
+      }}>
+        <Header/>
+        <Box sx={{
+          flexGrow: 1,
+          border: '1px solid blue',
+        }}>
+          <TaskListPanel
+            tasks={tasks}
+            onCreateTask={(details) => dispatch({ type: 'create', details })}
+            onUpdateTask={(task) => dispatch({ type: 'update', task })}
+            onDeleteTask={(id) => dispatch({ type: 'delete', id })}
+          />
+        </Box>
+        <Footer/>
       </Container>
     </ThemeProvider>
   );
---
+++ src/Footer.tsx
@@ -0,0 +1,13 @@
+import { Fab, Box } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+
+
+export const Footer = () => {
+  return (
+    <Box>
+      <Fab color="primary">
+        <AddIcon />
+      </Fab>
+    </Box>
+  );
+};
